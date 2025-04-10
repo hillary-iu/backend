6a60586fa99dc6bca3264884237afe146d14567a
hillary <hillary.eduarda@estudante.ifro.edu.br>
2025-04-10 10:28:31 -0400

    first commit

---
+++ exeercicio_01.js
@@ -0,0 +1,19 @@
+//exercicio 1
+let nome = "hillary"
+let idade = "15"
+
+console.log("ola, meu nome é", nome, "eu tenho", idade, "anos")
+
+//exercicio2
+let textoMinusculo = texto.lowerCase();
+let textoMaiusculo = texto.toupperCase();
+
+//exercicio 3 
+let nome2 = "hillary"
+console.log(typeof nome2)
+
+//exercicio4
+let peso= 70;
+let altura = 1.75;
+let imc = (peso / (altura * altura)).toFixed(2)
+console.log(`Seu IMC é: ${IMC}`)
